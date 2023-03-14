7512caad41a5e941ad2680bb445085769b5a0e49
HagerAlwe <125517587+HagerAlwe@users.noreply.github.com>
2023-03-14 17:27:22 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// window.alert("1-The first letter of the name must be uppercase") ;
+// window.alert("2- The first letter of the password must be a capital");
+// function nots() {
+
+//   let use =document.getElementById("User");
+
+//   if (use.value[0] == use.value[0].toUpperCase()){
+
+//     document.write("true")
+//   }else{
+//     document.write("false")
+//   }
+
+
+//   let pass =document.getElementById("pass");
+
+//   if (pass.value[0] == pass.value[0].toUpperCase()){
+
+//     document.write("true")
+//   }else{
+//     document.write("false")
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.alert("1-The first letter of the name must be uppercase") ;
+window.alert("2- The first letter of the password must be a capital");
+function nots() {
+
+  let use =document.getElementById("User");
+  let pass =document.getElementById("pass");
+  if (use.value[0] == use.value[0].toUpperCase()){
+    document.write("true")
+  }else if (pass.value[0] == pass.value[0].toUpperCase()){
+    document.write("true")
+  }
+    else{
+    document.write("false")
+
+    }
+  }
+
+
+
+
+
+
